504f4d8bfca9780b6d7463f5877dda4ccc54ab93
bona <bhayou.essega@gmail.com>
2022-03-10 14:03:40 +0700

    size chart pie

--- application/views/rangkuman/chart.php
+++ application/views/rangkuman/chart.php
@@ -16,10 +16,10 @@
             <div class="x_content">
                 <div class="clearfix"></div>
                 <div class="row">
-                    <div class="col-md-6 col-sm-6  ">
+                    <div class="col-md-12 col-sm-12  ">
                         <div class="x_panel">
                             <div class="x_title">
-                                <h2>Jumlah Kecelakaan</h2>
+                                <h2>Chart Pie</h2>
                                 <ul class="nav navbar-right panel_toolbox">
                                     <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                     </li>
