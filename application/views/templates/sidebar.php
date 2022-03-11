aa7085ccf478c568b74e69bd72b3a614ea4ef95f
BayuKun <bhayou.essega@gmail.com>
2022-03-11 20:02:36 +0700

    level user

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -23,44 +23,91 @@
 
             <br />
             <!-- sidebar menu -->
-            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-              <div class="menu_section">
-                <h3>Menu</h3>
-                <ul class="nav side-menu">
-                  <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="<?= base_url('Dashboard'); ?>">Dashboard</a></li>
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-edit"></i> Kecamatan <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="<?= base_url('Kecamatan'); ?>">Data Kecamatan</a></li>
-                      <li><a href="<?= base_url('Kecamatan/FormInput'); ?>">Input Kecamatan</a></li>
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-desktop"></i> Kasus <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="<?= base_url('Kasus'); ?>">Data Kasus</a></li>
-                      <li><a href="<?= base_url('Kasus/FormInput');  ?>">Input Kasus</a></li>
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-table"></i> Rangkuman <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="<?= base_url('Rangkuman/peta');  ?>">Peta</a></li>
-                      <li><a href="<?= base_url('Rangkuman/chart');  ?>">Chart</a></li>
-                      <li><a href="<?= base_url('Rangkuman/batang');  ?>">Diagram Batang</a></li>
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-user"></i> Pengguna <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="<?= base_url('Auth/pengguna');  ?>">Data Pengguna</a></li>
-                      <li><a href="<?= base_url('Auth/FormInput');  ?>">Input Pengguna</a></li>
-                    </ul>
-                  </li>
-                </ul>
+            <!-- Akses Menu Sesuai Level -->
+            <!-- Untuk admin -->
+            <?php if ($this->session->userdata('level')  == 1) { ?>
+              <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+                <div class="menu_section">
+                  <h3>Menu</h3>
+                  <ul class="nav side-menu">
+                    <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                      <ul class="nav child_menu">
+                        <li><a href="<?= base_url('Dashboard'); ?>">Dashboard</a></li>
+                      </ul>
+                    </li>
+                    <li><a><i class="fa fa-edit"></i> Kecamatan <span class="fa fa-chevron-down"></span></a>
+                      <ul class="nav child_menu">
+                        <li><a href="<?= base_url('Kecamatan'); ?>">Data Kecamatan</a></li>
+                        <li><a href="<?= base_url('Kecamatan/FormInput'); ?>">Input Kecamatan</a></li>
+                      </ul>
+                    </li>
+                    <li><a><i class="fa fa-desktop"></i> Kasus <span class="fa fa-chevron-down"></span></a>
+                      <ul class="nav child_menu">
+                        <li><a href="<?= base_url('Kasus'); ?>">Data Kasus</a></li>
+                        <li><a href="<?= base_url('Kasus/FormInput');  ?>">Input Kasus</a></li>
+                      </ul>
+                    </li>
+                    <li><a><i class="fa fa-table"></i> Rangkuman <span class="fa fa-chevron-down"></span></a>
+                      <ul class="nav child_menu">
+                        <li><a href="<?= base_url('Rangkuman/peta');  ?>">Peta</a></li>
+                        <li><a href="<?= base_url('Rangkuman/chart');  ?>">Chart</a></li>
+                        <li><a href="<?= base_url('Rangkuman/batang');  ?>">Diagram Batang</a></li>
+                      </ul>
+                    </li>
+                    <li><a><i class="fa fa-user"></i> Pengguna <span class="fa fa-chevron-down"></span></a>
+                      <ul class="nav child_menu">
+                        <li><a href="<?= base_url('Auth/pengguna');  ?>">Data Pengguna</a></li>
+                        <li><a href="<?= base_url('Auth/FormInput');  ?>">Input Pengguna</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
               </div>
+              <!-- Akses Petugas -->
+            <?php }
+            if ($this->session->userdata('level') == 2) { ?>
+              <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+                <div class="menu_section">
+                  <h3>Menu</h3>
+                  <ul class="nav side-menu">
+                    <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                      <ul class="nav child_menu">
+                        <li><a href="<?= base_url('Dashboard'); ?>">Dashboard</a></li>
+                      </ul>
+                    </li>
+                    <li><a><i class="fa fa-desktop"></i> Kasus <span class="fa fa-chevron-down"></span></a>
+                      <ul class="nav child_menu">
+                        <li><a href="<?= base_url('Kasus'); ?>">Data Kasus</a></li>
+                        <li><a href="<?= base_url('Kasus/FormInput');  ?>">Input Kasus</a></li>
+                      </ul>
+                    </li>
+                    <li><a><i class="fa fa-table"></i> Rangkuman <span class="fa fa-chevron-down"></span></a>
+                      <ul class="nav child_menu">
+                        <li><a href="<?= base_url('Rangkuman/peta');  ?>">Peta</a></li>
+                        <li><a href="<?= base_url('Rangkuman/chart');  ?>">Chart</a></li>
+                        <li><a href="<?= base_url('Rangkuman/batang');  ?>">Diagram Batang</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <!-- Akses Masyarakat -->
+            <?php }
+            if ($this->session->userdata('level') == 3) { ?>
+              <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+                <div class="menu_section">
+                  <h3>Menu</h3>
+                  <ul class="nav side-menu">
+                    <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                      <ul class="nav child_menu">
+                        <li><a href="<?= base_url('Dashboard'); ?>">Dashboard</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            <?php }; ?>
 
-            </div>
             <!-- /sidebar menu -->
 
             <!-- /menu footer buttons -->
